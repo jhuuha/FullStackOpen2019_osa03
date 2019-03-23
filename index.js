cd0dd05882c7b02a5e446b896c61817adb7e1111
Jarkko Huuha <jarkko.huuha@gmail.com>
2019-03-23 22:50:33 +0200

    3.4: puhelinluettelon backend step4

--- index.js
+++ index.js
@@ -42,6 +42,12 @@ app.get('/api/persons/:id', (req, res) => {
     }
 })
 
+app.delete('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id);
+    persons = persons.filter(n => n.id !== id);
+    res.status(204).end();
+});
+
 const PORT = 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
