bf042b0b758cf6d1ea7141e25e5fe3a34b4def95
Jarkko Huuha <jarkko.huuha@gmail.com>
2019-03-24 09:30:31 +0200

    3.9 puhelinluettelon backend step9

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
 const bodyParser = require('body-parser')
 const morgan = require('morgan')
 
+app.use(cors())
 app.use(bodyParser.json())
-
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :data'))
 
 let persons = [
