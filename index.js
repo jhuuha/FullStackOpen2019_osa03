830caf048f477ec371a529db893d58d8bc065864
Jarkko Huuha <jarkko.huuha@gmail.com>
2019-03-24 23:15:28 +0200

    3.19: puhelinluettelo ja tietokanta, step7

--- index.js
+++ index.js
@@ -60,14 +60,6 @@ app.post('/api/persons', (req, res, next) => {
         })
     }
 
-    /*
-    if (persons.find(n => n.name === body.name)) {
-        return res.status(400).json({
-            error: 'name must be unique'
-        })
-    }
-    */
-
     const person = new Person({
         name: body.name,
         number: body.number
@@ -114,6 +106,8 @@ const errorHandler = (error, req, res, next) => {
     console.error(error.message)
     if (error.name === 'CastError' && error.kind == 'ObjectId') {
         return res.status(400).send({ error: 'malformatted id' })
+    } else if (error.name === 'ValidationError') {
+        return res.status(400).json({ error: error.message })
     }
     next(error)
 }
--- models/person.js
+++ models/person.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const uniqueValidator = require('mongoose-unique-validator')
 
 mongoose.set('useFindAndModify', false)
 
@@ -15,9 +16,16 @@ mongoose.connect(url, { useNewUrlParser: true })
     })
 
 const personSchema = new mongoose.Schema({
-    name: String,
-    number: String,
+    name: {
+        type: String,
+        unique: true
+    },
+    number: {
+        type: String
+    },
 })
+
+personSchema.plugin(uniqueValidator)
 
 personSchema.set('toJSON', {
     transform: (document, returnedObject) => {
