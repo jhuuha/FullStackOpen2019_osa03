56dbe13091b2f5458c00f001fce4bc61f4950a38
Jarkko Huuha <jarkko.huuha@gmail.com>
2019-03-24 10:20:25 +0200

    3.11 puhelinluettelo full stack

--- README.md
+++ README.md
@@ -3,4 +3,4 @@
 Full stack open 2019 - Tehtävät
 
 3.10 puhelinluettelon backend step10:
-[https://rocky-springs-99354.herokuapp.com/api/persons](https://rocky-springs-99354.herokuapp.com/api/persons)
+[https://rocky-springs-99354.herokuapp.com](https://rocky-springs-99354.herokuapp.com)
---
+++ build/precache-manifest.15f8c72b03a2bf44b367828a446ef52c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e97e96dc07052d35b2d6",
+    "url": "/static/css/main.65fc7edd.chunk.css"
+  },
+  {
+    "revision": "e97e96dc07052d35b2d6",
+    "url": "/static/js/main.8a3b6473.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "c46792c85541ce2ef4bb",
+    "url": "/static/js/2.372099d9.chunk.js"
+  },
+  {
+    "revision": "bd17b477abb093f1c89a2aa7faf0aa8b",
+    "url": "/index.html"
+  }
+];
