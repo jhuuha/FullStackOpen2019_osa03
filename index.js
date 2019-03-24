376cb95ebd59c4bb9096fde31091fb7dd02dfea0
Jarkko Huuha <jarkko.huuha@gmail.com>
2019-03-24 20:51:38 +0200

    3.16: puhelinluettelo ja tietokanta, step3

--- index.js
+++ index.js
@@ -42,10 +42,12 @@ app.get('/info', (req, res) => {
 })
 
 //hakee kokoelman kaikki resurssit
-app.get('/api/persons', (req, res) => {
-    Person.find({}).then(persons => {
-        res.json(persons)
-    })
+app.get('/api/persons', (req, res, next) => {
+    Person.find({})
+        .then(persons => {
+            res.json(persons)
+        })
+        .catch(error => next(error))
 })
 
 //hakee yksittäisen resurssin
@@ -60,7 +62,7 @@ app.get('/api/persons/:id', (req, res) => {
 })
 
 //luo uuden resurssin pyynnön mukana olavasta datasta
-app.post('/api/persons', (req, res) => {
+app.post('/api/persons', (req, res, next) => {
 
     const body = req.body
 
@@ -83,26 +85,40 @@ app.post('/api/persons', (req, res) => {
         name: body.name,
         number: body.number
     })
-    person.save().then(savedPerson => {
-        res.json(savedPerson.toJSON())
-    })
+    person.save()
+        .then(savedPerson => {
+            res.json(savedPerson.toJSON())
+        })
+        .catch(error => next(error))
 })
 
 //poistaa yksilöidyn resussin
-app.delete('/api/persons/:id', (req, res) => {
+app.delete('/api/persons/:id', (req, res, next) => {
     Person.findByIdAndRemove(req.params.id)
         .then(result => {
             res.status(204).end()
         })
-});
+        .catch(error => next(error))
+})
 
 
+//olemattomien osoitteiden käsittely
 const unknownEndpoint = (req, res) => {
     res.status(404).end()
 }
 app.use(unknownEndpoint)
 
+//virheellisten pyyntöjen käsittely
+const errorHandler = (error, req, res, next) => {
+    console.error(error.message)
+    if (error.name === 'CastError' && error.kind == 'ObjectId') {
+        return res.status(400).send({ error: 'malformatted id' })
+    }
+    next(error)
+}
+app.use(errorHandler)
 
+//serveri
 const PORT = process.env.PORT
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
